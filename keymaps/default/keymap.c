ccd0a3bf3ab794086c7611fc6ad4ffc005a9bb12
Bastian <73909061+bastifpv@users.noreply.github.com>
2023-09-25 12:32:37 +0200

    keymaps un dvia

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -6,7 +6,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_ortho_3x3(
-        KC_AUDIO_VOL_UP,    KC_AUDIO_VOL_DOWN,    KC_C,
+        KC_A,    KC_B,    KC_C,
         KC_D,    KC_E,    KC_F,
         KC_G,    KC_H,    KC_I
     )
